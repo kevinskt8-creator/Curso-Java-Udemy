47451705d51c774ae02fb77f891f23e51d75d324
kevinskt8-creator <kevinskt8@gmail.com>
2026-09-09 12:28:57 -0300

    Estruturas de Repetições

---
+++ index/src/Estruturasderepeticao/EstruturaDeRepeticao.java
@@ -0,0 +1,6 @@
+package Estruturasderepeticao;
+
+public class EstruturaDeRepeticao {
+    
+    
+}
---
+++ index/src/Exercicios/exercicios01.java
@@ -0,0 +1,24 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class exercicios01 {
+   
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int A, B, soma;
+
+		A = sc.nextInt();
+		B = sc.nextInt();
+
+		soma = A + B;
+
+		System.out.println("SOMA = " + soma);
+
+		sc.close();
+	}
+}
+    
+
