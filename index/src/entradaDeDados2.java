7062df5552eb48b7a60f44935192e6a1f82bb11d
kevinskt8-creator <kevinskt8@gmail.com>
2026-09-09 10:30:41 -0300

    Estrutura sequencial, Entrada de dados

---
+++ index/src/entradaDeDados2.java
@@ -0,0 +1,6 @@
+public class entradaDeDados2 {
+    
+    public static void main( String [] args) {
+         Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ index/src/estruturasequencial/ddd.java
@@ -0,0 +1,12 @@
+package estruturasequencial;
+public class ddd {
+    public static void main(String args[]) {
+        double a;
+        int b;
+        a = 5.0;
+        b = (int) a;
+        System.out.println(b);
+    }
+}
+
+// Usar Casting para informa o tipo de resultado que vc quer.
