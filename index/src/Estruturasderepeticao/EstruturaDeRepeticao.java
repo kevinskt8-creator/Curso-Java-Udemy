b779716cb4671e4e93bdded2edbf692fd64e43eb
kevinskt8-creator <kevinskt8@gmail.com>
2026-09-15 18:57:07 -0300

    ok

--- index/src/Estruturasderepeticao/EstruturaDeRepeticao.java
+++ index/src/Estruturasderepeticao/EstruturaDeRepeticao.java
@@ -1,6 +1,25 @@
 package Estruturasderepeticao;
 
+import java.util.Scanner;
+
 public class EstruturaDeRepeticao {
-    
-    
+    public static  void  main(String [] args){
+
+    var scanner = new Scanner(System.in);
+
+    System.out.println("Bem Vindo!");
+
+    var x = 7;
+    var z = 8;
+    int soma = x + z;
+
+    if ( x >= 10) {
+        
+        System.out.print(soma);
+    }
+    else {
+        System.out.print("X é maior que 10");
+    }
+    }
+ 
 }
