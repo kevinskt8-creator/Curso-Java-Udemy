b9204e021e71c895d33de24fc18952c7aef55d1e
kevinskt8-creator <kevinskt8@gmail.com>
2026-09-16 20:03:05 -0300

    While exercicios

---
+++ index/src/Exercicios/exercicio03.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class exercicio03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+
+        while (x != 2002) {
+            System.out.println("Senha invalida!");
+            x = sc.nextInt();
+          
+
+        }
+
+        System.out.println("Acesso permitido");
+          
+
+    }
+
+  
+
+}
