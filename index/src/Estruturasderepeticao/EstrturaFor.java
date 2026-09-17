a75c710e68a1d2b52f8a878df106b38713788a19
kevinskt8-creator <kevinskt8@gmail.com>
2026-09-17 15:29:27 -0300

    For exercicio

--- index/src/Estruturasderepeticao/EstrturaFor.java
+++ index/src/Estruturasderepeticao/EstrturaFor.java
@@ -6,18 +6,10 @@ public class EstrturaFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        for (int i=0; i<5; i++) {
+            System.out.println("Valor de i: " + i);
+        }
 
-        int soma = 0;
-
-        for (int i=0; i<N; i++);
-        
-        int x = sc.nextInt();
-
-        soma = soma + x;
-
-
-        sc.close();
     }
     
 }
---
+++ index/src/Exercicios/Exercicio04.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    Scanner sc = new Scanner(System.in);
+   
+
+    for (1 <=  X <=  1000)
+
+
+    
+}
